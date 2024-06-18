64ac16e618484685ad4ab3886807873e61a0e774
Rujal Manandhar <rujal@Rujals-MacBook-Pro.local>
2024-06-19 00:46:08 +0545

    [#2] Make JpaRepositoryConfigExtension get the Entity.class, MappedSuperClass.class from the same InspectionClassLoader if any agent is active - Also use invesdwin to hot attach an agent because modifying launch script is not working, and I already have spent a ton of my time for the MCVE

--- src/main/java/com/mcve/ltw/weaved/ProfilingAspect.java
+++ src/main/java/com/mcve/ltw/weaved/ProfilingAspect.java
@@ -3,26 +3,81 @@ package com.mcve.ltw.weaved;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.util.StopWatch;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.instrument.classloading.ShadowingClassLoader;
+import org.springframework.lang.Nullable;
+import org.springframework.util.ClassUtils;
+
+import java.util.*;
 
 @Aspect
 public class ProfilingAspect {
 
-	@Around("methodsToBeProfiled()")
-	public Object profile(ProceedingJoinPoint pjp) throws Throwable {
-		StopWatch sw = new StopWatch(getClass().getSimpleName());
-		try {
-			sw.start(pjp.getSignature().getName());
-			return pjp.proceed();
-		} finally {
-			sw.stop();
-			System.out.println(sw.prettyPrint());
+	private InspectionClassLoader inspectionClassLoader;
+
+	@Around("execution(* org.springframework.data.jpa.repository.config.JpaRepositoryConfigExtension.getConfigurationInspectionClassLoader(..)) && args(loader,..)")
+	public Object getConfigurationInspectionClassLoader(ProceedingJoinPoint pjp, ResourceLoader loader) {
+		ClassLoader classLoader = loader.getClassLoader();
+
+		if (classLoader != null && LazyJvmAgent.isActive(classLoader)) {
+			inspectionClassLoader = new InspectionClassLoader(classLoader);
+			return inspectionClassLoader;
+		}
+		return classLoader;
+	}
+
+	@Around("execution(* org.springframework.data.jpa.repository.config.JpaRepositoryConfigExtension.getIdentifyingAnnotations(..))")
+	public Object getIdentifyingAnnotations(ProceedingJoinPoint pjp) throws Throwable {
+		if (inspectionClassLoader != null) {
+			List<Class<?>> classes = Arrays.asList(inspectionClassLoader.loadClass("jakarta.persistence.Entity"),
+					inspectionClassLoader.loadClass("jakarta.persistence.MappedSuperclass"));
+			inspectionClassLoader = null; // Just setting it null to avoid unintended effects
+			return classes;
+		}
+		return pjp.proceed();
+	}
+
+	/**
+	 * This is the ditto implementation of {@link org.springframework.data.jpa.repository.config.InspectionClassLoader}.
+	 */
+	private static class InspectionClassLoader extends ShadowingClassLoader {
+
+		InspectionClassLoader(ClassLoader parent) {
+
+			super(parent, true);
+
+			excludePackage("org.springframework.");
 		}
 	}
 
-	@Pointcut("execution(public * com.mcve.ltw..*.*(..))")
-	public void methodsToBeProfiled(){
-		System.out.println("egewgerg");
+	/**
+	 * This is the ditto implementation of {@link org.springframework.data.jpa.repository.config.JpaRepositoryConfigExtension.LazyJvmAgent}.
+	 */
+	private static class LazyJvmAgent {
+
+		private static final Set<String> AGENT_CLASSES;
+
+		static {
+
+			Set<String> agentClasses = new LinkedHashSet<>();
+
+			agentClasses.add("org.springframework.instrument.InstrumentationSavingAgent");
+			agentClasses.add("org.eclipse.persistence.internal.jpa.deployment.JavaSECMPInitializerAgent");
+
+			AGENT_CLASSES = Collections.unmodifiableSet(agentClasses);
+		}
+
+		private LazyJvmAgent() {}
+
+		/**
+		 * Determine if any agent is active.
+		 *
+		 * @return {@literal true} if an agent is active.
+		 */
+		static boolean isActive(@Nullable ClassLoader classLoader) {
+
+			return AGENT_CLASSES.stream() //
+					.anyMatch(agentClass -> ClassUtils.isPresent(agentClass, classLoader));
+		}
 	}
 }
